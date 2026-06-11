0eb73bfe16e489610cec441e12d39bfa09c6807c
Stas Bekman <stas.bekman@snowflake.com>
2026-06-11 21:26:14 +0000

    tests
    Signed-off-by: Stas Bekman <stas.bekman@snowflake.com>

---
+++ tests/test_tmp.py
@@ -0,0 +1,5 @@
+from arctic_platform.rl import ArcticRLClientConfig, create_arctic_rl_client
+from arctic_platform.rl.server import ArcticRLServerState
+
+def test_import():
+    pass # just test for now we can import the symbols used by integrations 
